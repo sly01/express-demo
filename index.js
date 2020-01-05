5d69440c2081ace9135832db7ed75ef873be064f
sly01 <aerkoc01@gmail.com>
2020-01-05 13:05:15 +0100

    Added env check for morgan to log every request

--- index.js
+++ index.js
@@ -9,7 +9,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.use(helmet());
-app.use(morgan('tiny'));
+
+if (app.get('env') === 'development') {
+    app.use(morgan('tiny'));
+    console.log("Morgan Enabled");
+}
 app.use(logger);
 
 const courses = [
