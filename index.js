bedd98d3e80b1b0e8c359be4817ffaecbda3e6c8
sly01 <aerkoc01@gmail.com>
2020-01-05 15:02:17 +0100

    Added config management(dev,prod,staging)

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const config = require('config');
 const helmet = require('helmet');
 const morgan = require('morgan');
 const Joi = require('joi');
@@ -9,6 +10,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.use(helmet());
+
+console.log("App name:", config.get('name'));
+console.log("Mail Host:", config.get('mail.host'));
+console.log("Mail Pass:", config.get('mail.password'));
 
 if (app.get('env') === 'development') {
     app.use(morgan('tiny'));
