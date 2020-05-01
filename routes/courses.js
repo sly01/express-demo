15164e12d807edeb29d3adb676f01e300becf09d
sly01 <aerkoc01@gmail.com>
2020-05-01 13:47:17 +0200

    Code format

--- routes/courses.js
+++ routes/courses.js
@@ -2,9 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 const courses = [
-    { id:1, name: 'course1'},
-    { id:2, name: 'course2'},
-    { id:3, name: 'course3'}
+    { id: 1, name: 'course1' },
+    { id: 2, name: 'course2' },
+    { id: 3, name: 'course3' }
 ];
 
 router.get('/', (req, res) => {
@@ -12,10 +12,10 @@ router.get('/', (req, res) => {
 });
 
 router.post('/', (req, res) => {
-    const {error} = validateCourse(req.body);
+    const { error } = validateCourse(req.body);
 
     if (error) return res.status(400).send(error.details[0].message);
-    
+
     const course = {
         id: courses.length + 1,
         name: req.body.name
@@ -25,13 +25,13 @@ router.post('/', (req, res) => {
 });
 
 router.put('/:id', (req, res) => {
-    const course = courses.find( c => c.id === parseInt(req.params.id));
+    const course = courses.find(c => c.id === parseInt(req.params.id));
     if (!course) return res.status(404).send('The course with the given ID was not found');
-    
-    const {error} = validateCourse(req.body);
+
+    const { error } = validateCourse(req.body);
 
     if (error) return res.status(400).send(error.details[0].message);
-    
+
     course.name = req.body.name;
     res.send(course)
 });
@@ -43,7 +43,7 @@ router.get('/:id', (req, res) => {
 });
 
 router.delete('/:id', (req, res) => {
-    const course = courses.find( c => c.id === parseInt(req.params.id));
+    const course = courses.find(c => c.id === parseInt(req.params.id));
     if (!course) return res.status(404).send('The course with the given ID was not found');
 
     const index = courses.indexOf(course);
--- routes/home.js
+++ routes/home.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.render('index', {title: "My Express App", message: "Hello Message"});
+    res.render('index', { title: "My Express App", message: "Hello Message" });
 });
 
 module.exports = router;
